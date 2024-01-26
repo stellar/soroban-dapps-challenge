9df73e9d04459c49a502cfc2fd21043908183254
Julian Martinez <julian_martinez28@outlook.com>
2024-01-26 13:48:44 -0800

    Squashed commit of the following:
    commit d03beb0451ed38770f6f334809cd83a414682729
    Author: Julian Martinez <julian_martinez28@outlook.com>
    Date:   Fri Jan 26 13:47:58 2024 -0800
    
        working

--- hooks/useAccount.ts
+++ hooks/useAccount.ts
@@ -1,123 +1,49 @@
-import { useEffect, useState } from 'react'
-import { useAppContext } from '../context/appContext'
-import {
-  ISupportedWallet,
-  StellarWalletsKit,
-  WalletNetwork,
-  WalletType,
-} from 'stellar-wallets-kit/build/module'
+import { useEffect, useState } from "react";
+import { isConnected, getUserInfo } from "@stellar/freighter-api";
+
+let address: string;
+
+let addressLookup = (async () => {
+  if (await isConnected()) return getUserInfo()
+})();
 
 // returning the same object identity every time avoids unnecessary re-renders
 const addressObject = {
   address: '',
   displayName: '',
-}
+};
 
 const addressToHistoricObject = (address: string) => {
-  addressObject.address = address
-  addressObject.displayName = `${address.slice(0, 4)}...${address.slice(-4)}`
+  addressObject.address = address;
+  addressObject.displayName = `${address.slice(0, 4)}...${address.slice(-4)}`;
   return addressObject
-}
+};
 
-// Soroban is only supported on Futurenet right now
-const FUTURENET_DETAILS = {
-  network: 'FUTURENET',
-  networkUrl: 'https://horizon-futurenet.stellar.org',
-  networkPassphrase: 'Test SDF Future Network ; October 2022',
-}
+/**
+ * Returns an object containing `address` and `displayName` properties, with
+ * the address fetched from Freighter's `getPublicKey` method in a
+ * render-friendly way.
+ *
+ * Before the address is fetched, returns null.
+ *
+ * Caches the result so that the Freighter lookup only happens once, no matter
+ * how many times this hook is called.
+ *
+ * NOTE: This does not update the return value if the user changes their
+ * Freighter settings; they will need to refresh the page.
+ */
+export function useAccount(): typeof addressObject | null {
+  const [, setLoading] = useState(address === undefined);
 
-const ERRORS = {
-  WALLET_CONNECTION_REJECTED: 'Wallet connection rejected',
-}
-
-const STORAGE_WALLET_KEY = 'wallet'
-
-const allowedWallets = [
-  // WalletType.ALBEDO,
-  WalletType.FREIGHTER,
-  // WalletType.XBULL,
-]
-
-type Props = {
-  account: typeof addressObject | null
-  onConnect: () => void
-  onDisconnect: () => void
-  isLoading: boolean
-}
-export function useAccount(): Props {
-  const { walletAddress, setWalletAddress } = useAppContext()
-
-  const [isLoading, setIsLoading] = useState(false)
-
-  // Update is not only Futurenet is available
-  const [selectedNetwork] = useState(FUTURENET_DETAILS)
-  // Setup swc, user will set the desired wallet on connect
-  const [SWKKit] = useState(
-    new StellarWalletsKit({
-      network: selectedNetwork.networkPassphrase as WalletNetwork,
-      selectedWallet: WalletType.FREIGHTER,
-    })
-  )
-
-  const getWalletAddress = async (type: WalletType) => {
-    try {
-      setIsLoading(true)
-      // Set selected wallet, network, and public key
-      SWKKit.setWallet(type)
-      const publicKey = await SWKKit.getPublicKey()
-      await SWKKit.setNetwork(WalletNetwork.FUTURENET)
-
-      // Short timeout to prevent blick on loading address
-      setTimeout(() => {
-        setWalletAddress(publicKey)
-        localStorage.setItem(STORAGE_WALLET_KEY, type)
-        setIsLoading(false)
-      }, 500)
-    } catch (error) {
-      localStorage.removeItem(STORAGE_WALLET_KEY)
-      setIsLoading(false)
-      console.error(ERRORS.WALLET_CONNECTION_REJECTED)
-    }
-  }
-
-  // if the walletType is stored in local storage the first opening the page
-  // will trigger autoconnect for users
   useEffect(() => {
-    const storedWallet = localStorage.getItem(STORAGE_WALLET_KEY)
-    if (
-      !walletAddress &&
-      storedWallet &&
-      Object.values(WalletType).includes(storedWallet as WalletType)
-    ) {
-      ;(async () => {
-        await getWalletAddress(storedWallet as WalletType)
-      })()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [walletAddress])
+    if (address !== undefined) return;
 
-  const onConnect = async () => {
-    if (!walletAddress) {
-      // See https://github.com/Creit-Tech/Stellar-Wallets-Kit/tree/main for more options
-      await SWKKit.openModal({
-        allowedWallets,
-        onWalletSelected: async (option: ISupportedWallet) => {
-          await getWalletAddress(option.type)
-        },
-      })
-    }
-  }
+    addressLookup
+      .then(user => { if (user) address = user.publicKey })
+      .finally(() => { setLoading(false) });
+  }, []);
 
-  const onDisconnect = () => {
-    setWalletAddress('')
-    localStorage.removeItem(STORAGE_WALLET_KEY)
-    setIsLoading(false)
-  }
+  if (address) return addressToHistoricObject(address);
 
-  return {
-    account: walletAddress ? addressToHistoricObject(walletAddress) : null,
-    onConnect,
-    onDisconnect,
-    isLoading,
-  }
-}
+  return null;
+};
