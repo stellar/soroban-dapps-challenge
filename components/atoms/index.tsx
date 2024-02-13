dbd581cbc147685c910b85bcb194c45cc1371f69
Julian Martinez <julian_martinez28@outlook.com>
2024-02-13 08:24:23 -0800

    update-boilerplate

--- components/atoms/index.tsx
+++ components/atoms/index.tsx
@@ -1,4 +1,3 @@
 export * from "./card";
 export * from "./button";
 export * from "./connect-button";
-export * from "./loading";
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import {  } from "";
+import {  } from "";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h3>Soroban Quickstart Dapp</h3>
+        < />
+      </header>
+      <main className={styles.main}>
+        <div className={styles.content}>
+          < />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
