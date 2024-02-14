56387ba19a8edd3d188b134d46d4decbce820895
Julian Martinez <julian_martinez28@outlook.com>
2024-02-14 12:45:48 -0800

    remove pre-filled data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { HelloWorld } from "../components/organisms";
-import { WalletData } from "../components/molecules";
+import {  } from "../components/organisms";
+import {  } from "../components/molecules";
 
 const Home: NextPage = () => {
   return (
     <>
       <header className={styles.header}>
         <h3>Soroban Quickstart Dapp</h3>
-        <WalletData />
+        < />
       </header>
       <main className={styles.main}>
         <div className={styles.content}>
-          <HelloWorld />
+          < />
         </div>
       </main>
     </>
