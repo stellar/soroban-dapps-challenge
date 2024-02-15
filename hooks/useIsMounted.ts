6c37bdfdcd4950a9bddd9d24589d8b31973ea652
Julian Martinez <julian_martinez28@outlook.com>
2024-02-14 17:01:13 -0800

    Update useIsMounted.ts

--- hooks/useIsMounted.ts
+++ hooks/useIsMounted.ts
@@ -1,5 +1,9 @@
 import React from "react";
 
+/**
+ * Custom hook that returns a boolean value indicating whether the component is mounted or not.
+ * @returns {boolean} A boolean value indicating whether the component is mounted.
+ */
 export function useIsMounted() {
   const [mounted, setMounted] = React.useReducer(() => true, false);
   React.useEffect(setMounted, [setMounted]);
