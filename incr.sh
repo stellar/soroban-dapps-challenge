87c6d9e2e73cfca7239779612d6232de80f88dac
Julian Martinez <julian_martinez28@outlook.com>
2024-02-21 07:59:10 -0800

    update scripts

--- incr.sh
+++ incr.sh
@@ -1,10 +1,11 @@
 EXAMPLE_USER_SECRET="$(soroban config identity show example-user)"
+NETWORK="$(cat ./.soroban-example-dapp/network)"
 CONTRACT_ID="$(cat ./.soroban/hello_world_id)"
 
 soroban contract invoke \
---network testnet \
---source $EXAMPLE_USER_SECRET \
---id $CONTRACT_ID \
--- \
-increment \
---incr 7
+    --network $NETWORK \
+    --source $EXAMPLE_USER_SECRET \
+    --id $CONTRACT_ID \
+    -- \
+    increment \
+    --incr 9
